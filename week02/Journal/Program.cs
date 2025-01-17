e8dcadb0fd8089f4626af8e313a5b23fd907fe4c
Arkangel111 <147678654+Arkangel111@users.noreply.github.com>
2025-01-16 22:30:01 -0500

    working top level loop

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,7 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
+        while (true)
+        {
+            Console.WriteLine("1. Write new entry");
+            Console.WriteLine("2. display");
+            Console.WriteLine("3. save");
+            Console.WriteLine("4. load");
+            Console.WriteLine("5. quit");
 
+            Console.Write("enter seelction: ");
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine($"user selected {userInput}");
+        }
     }
 
 }
