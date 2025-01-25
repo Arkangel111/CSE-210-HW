dfa1597d61a753cc45e12964bf8750c51760783d
Arkangel111 <147678654+Arkangel111@users.noreply.github.com>
2025-01-24 19:36:50 -0500

    Reference started

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -8,26 +8,16 @@ class Reference
 
     public Reference(string book, int chapter, int verse)
     {
-        _book = book;
-        _chapter = chapter;
-        _verse = verse;
-        _endVerse = -1;
+
     }
 
     public Reference(string book, int chapter, int startVerse, int endVerse)
     {
-        _book = book;
-        _chapter = chapter;
-        _verse = startVerse;
-        _endVerse = endVerse;
+
     }
 
     public string GetDisplayText()
     {
-        if (_endVerse == -1)
-        {
-            return $"{_book} {_chapter}:{_verse}";
-        }
-        return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+        return "";
     }
 }
