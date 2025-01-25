61e25edda15210be0866a436b0ba4a00bff2fdf1
Arkangel111 <147678654+Arkangel111@users.noreply.github.com>
2025-01-24 19:34:39 -0500

    scripture start

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,45 +1,30 @@
 using System;
-class Scripture
+
+public class Scripture
 {
     private Reference _reference;
     private List<Word> _words;
 
     public Scripture(Reference reference, string text)
     {
-        _reference = reference;
-        _words = text.Split(' ').Select(word => new Word(word)).ToList();
+
     }
 
-    public void HideRandomWords(int numberToHide)
+    public void HideRandomWords(int numbertohide)
     {
-        Random random = new Random();
-        int hiddenCount = 0;
 
-        while (hiddenCount < numberToHide && _words.Any(word => !word.IsHidden()))
-        {
-            int index = random.Next(_words.Count);
-            if (!_words[index].IsHidden())
-            {
-                _words[index].Hide();
-                hiddenCount++;
-            }
-        }
     }
 
     public string GetDisplayText()
     {
-        string referenceText = _reference.GetDisplayText();
-        string wordsText = string.Join(" ", _words.Select(word => word.GetDisplayText()));
-        return $"{referenceText} {wordsText}";
+        return "";
     }
 
     public bool IsCompletelyHidden()
     {
-        return _words.All(word => word.IsHidden());
+        return false;
     }
+
 }
-
-
-
 
 3 john 5:13
