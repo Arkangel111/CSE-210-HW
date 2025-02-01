272e8d3c422e2cb621c56f56a7ec03e4e9992db8
Arkangel111 <147678654+Arkangel111@users.noreply.github.com>
2025-02-01 18:50:25 -0500

    comment class fleshed out

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -4,4 +4,20 @@ public class Comment
 {
     private string _username;
     private string _text;
+
+    public Comment(string username, string text)
+    {
+        _username = username;
+        _text = text;
+    }
+
+    public string GetUsername()
+    {
+        return _username;
+    }
+
+    public string GetText()
+    {
+        return _text;
+    }
 }
