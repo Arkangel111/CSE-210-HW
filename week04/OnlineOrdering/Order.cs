0e73f30f7f05b7aa3375dbfd438ef95d0ff5e7e7
Arkangel111 <147678654+Arkangel111@users.noreply.github.com>
2025-02-01 21:25:51 -0500

    slowly working it

--- week04/OnlineOrdering/Order.cs
+++ week04/OnlineOrdering/Order.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 public class Order
 {
     private Customer _customer;
-    private Address _address;
-    private Product _product;
+    private List<Product> _products;
+    private const decimal US_Rate = 5;
+    private const decimal EU_Rate = 35;
 
+
+    public Order(Customer customer, List<Product> products)
+    {
+        _customer = customer;
+        _products = products;
+    }
+
+    public decimal TotalCost()
+    {
+        decimal prodTotal = 0;
+
+        foreach (var product in _products)
+        {
+            prodTotal += product.TotalCost();
+        }
+
+        if (_customer.InUSA())
+        {
+            return prodTotal + (prodTotal * US_Rate / 100);
+        }
+
+        else
+        {
+            return prodTotal + (prodTotal * EU_Rate / 100);
+        }
+
+
+        public string GetPackingLabel()
+    {
+        string label = "Label:\n";
+        foreach (var product in _products)
+        {
+            label += $"{product.GetProductName()}  (ID: {product.GetProductID()})\n";
+        }
+    }
+}
 }
--- week04/OnlineOrdering/Product.cs
+++ week04/OnlineOrdering/Product.cs
@@ -2,17 +2,41 @@ using System;
 
 public class Product
 {
-    private string _productname;
-    private string _productid;
+    private string _productName;
+    private string _productID;
     private int _quantity;
     private int _price;
 
+    public Product(string productname, string productid, int quantity, int price)
+    {
+        _productName = productname;
+        _productID = productid;
+        _quantity = quantity;
+        _price = price;
+    }
 
     public int TotalCost(int _quantity, int _price)
     {
-        int quantity = _quantity;
-        int price = _price;
-        return quantity * price;
-
+        return _quantity * _price;
     }
+
+    public string GetProductName()
+    {
+        return _productName;
+    }
+
+    public string GetProductID()
+    {
+        return _productID;
+    }
+
+    public void DisplayProduct()
+    {
+        Console.WriteLine("Product Name: " + _productName);
+        Console.WriteLine("Product ID: " + _productID);
+        Console.WriteLine("Quantity: " + _quantity);
+        Console.WriteLine("Price: " + _price);
+        Console.WriteLine($"Total Cost: {TotalCost(_quantity, _price)}");
+    }
+
 }
