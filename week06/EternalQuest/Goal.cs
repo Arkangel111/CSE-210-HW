68c1c5fbc99a7611302aacf992f51283c962ed7e
Arkangel111 <147678654+Arkangel111@users.noreply.github.com>
2025-02-14 16:01:27 -0500

    goal and goal manager skeleton started

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,33 @@
+using System;
+
+public class Goal
+{
+    private string _shortName;
+    private string _description;
+    private string _points;
+
+    public Goal(string name, string description, string points)
+    {
+        return;
+    }
+
+    public void RecordEvent()
+    {
+        return;
+    }
+
+    public bool IsComplete()
+    {
+        return false;
+    }
+
+    public string GetDetailsString()
+    {
+        return "";
+    }
+
+    public string GetStringRepresentation()
+    {
+        return "";
+    }
+}
---
+++ week06/EternalQuest/GoalManager.cs
@@ -0,0 +1,53 @@
+using System;
+
+public class GoalManager
+{
+    private List<Goal> _goals;
+    private int _score;
+
+    public GoalManager()
+    {
+        return;
+    }
+
+    public void Start()
+    {
+        return;
+    }
+
+    public void DisplayPlayerInfo()
+    {
+        return;
+    }
+
+    public void ListGoalNames()
+    {
+        return;
+    }
+
+    public void ListGoalDetails()
+    {
+        return;
+    }
+
+    public void CreateGoal()
+    {
+        return;
+    }
+
+    public void RecordEvent()
+    {
+        return;
+    }
+
+    public void SaveGoals()
+    {
+        return;
+    }
+
+    public void LoadGoals()
+    {
+        return;
+    }
+
+}
