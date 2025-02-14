8dc4adf2f224b395e4540f4296a0db2a5348263a
Arkangel111 <147678654+Arkangel111@users.noreply.github.com>
2025-02-14 16:31:49 -0500

    EternalGoal skeleton

--- week06/EternalQuest/EternalGoal.cs
+++ week06/EternalQuest/EternalGoal.cs
@@ -1,1 +1,23 @@
 using System;
+
+public class EternalGoal(string name, string description, string points)
+{
+    _shortName = name;
+    _description = description;
+    _points = points;
+
+        public override void RecordEvent()
+    {
+        return;
+    }
+
+    public override bool IsComplete()
+    {
+        return false;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return "";
+    }
+}
